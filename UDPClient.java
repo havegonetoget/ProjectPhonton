884df71639d36d10cd9c71b5428e6c08cdd7e27d
Nwherring <nicholasherring@bellsouth.net>
2025-02-14 17:47:50 -0600

    Added an option to change Networks

--- UDPClient.java
+++ UDPClient.java
@@ -4,14 +4,29 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
+import java.util.Scanner;
 //UDP client to send game-related messages over the network
 public class UDPClient {
-    private static final String BROADCAST_IP = "127.0.0.1"; // Localhost
-    private static final int BROADCAST_PORT = 7500;
+    private static String BROADCAST_IP = "127.0.0.1"; // Localhost
+    private static int BROADCAST_PORT = 7500;
 
     //send a message via udp to the specified brodcast port
     public static void sendMessage(String message) throws IOException {
+
+        //Check if the user would like the change IP networks
+        //Create scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        //ask if user would like to change IP
+        System.out.println("Would you like to change networks (Y/N)");
+        String comparator = scanner.nextLine();
+
+        //if the user decides to change the IP
+        if (comparator.compareToIgnoreCase("Y") == 0){
+            System.out.println("Please input the new IP network");
+            BROADCAST_IP = scanner.nextLine();
+        }
+
         //create a udp socket for sending data
         DatagramSocket socket = new DatagramSocket();
         InetAddress ip = InetAddress.getByName(BROADCAST_IP);
