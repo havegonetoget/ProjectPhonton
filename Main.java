8d39dd8d6df2ac1a6095da635f53be22118f2867
Van VanNorman <134340101+havegonetoget@users.noreply.github.com>
2025-04-16 00:36:02 -0500

    removed commented out code

--- Main.java
+++ Main.java
@@ -22,15 +22,7 @@ public class Main {
                 e.printStackTrace();
             }
         }
-       /* Start the UDP Server in a separate thread
-           new Thread(() -> {
-            try {
-                UDPServer.main(new String[0]);  // Start UDP Server
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start(); */
-
+      
         
         // Show the splash screen
         SplashScreen.showSplashScreen();
@@ -54,39 +46,8 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-   
-
          
     }
 
-//This part is not integral at the moment of working the project 
-/*    public static void startGameSim() {
-        new Thread(() -> {
-            runPythonTrafficGenerator();  // This method will run the Python script
-        }).start(); 
-    }
 
-    // Method to run the Python game simulator (traffic generator)
-    private static void runPythonTrafficGenerator() {
-        try {
-            // Command to run the Python script (ensure Python is installed and added to the system's PATH)
-            ProcessBuilder builder = new ProcessBuilder("python3", "python_trafficgenarator_v2.py"); // Adjust the path if necessary
-            builder.redirectErrorStream(true);  // Merge stdout and stderr
-
-            // Start the process
-            Process process = builder.start();
-
-            // Read the output from the Python script
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("Python Simulator Output: " + line);
-            }
-
-            // Wait for the Python script to finish 
-            process.waitFor();  // This line ensures the main method waits for the Python script to finish
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } */
 }
